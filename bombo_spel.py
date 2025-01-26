770721e8041b5e87bcca6484f36cace083388173
Mathijs Bakker <204105@stmichaelcollege.nl>
2025-01-26 19:36:26 +0100

    timer en scorebord toegevoegd

--- bombo_spel.py
+++ bombo_spel.py
@@ -19,14 +19,35 @@ pygame.display.set_caption("Screen Loop Example")
 # Clock for controlling the frame rate
 clock = pygame.time.Clock()
 
+
+#score vooral van https://www.youtube.com/watch?app=desktop&v=Fp1dudhdX8k&t=0s
+font = pygame.font.SysFont(None, 32)
+
+score_value = 0
+timeboard = 0
+
+textX = 10
+textY = 10
+
+tijdX = 700
+tijdY = 10
+def show_score(x,y):
+    score = font.render("Score : " + str(score_value), True, (255, 0, 255))
+    screen.blit(score, (x,y))
+    
+#tijd
+    am_ticks = 0
+def show_tijd(x,y):
+    tijd = font.render("tijd : " + str(am_ticks), True, (255, 0, 255))
+    screen.blit(tijd, (x,y))
 # Player setup
 player_size = 50
-player_x = SCREEN_WIDTH // 2
+player_x = SCREEN_WIDTH // 3
 player_y = SCREEN_HEIGHT // 2
 player_speed = 10
 
 bombo_size = 100
-bombo_x = SCREEN_WIDTH // 2
+bombo_x = SCREEN_WIDTH // 1.5
 bombo_y = SCREEN_HEIGHT // 2
 bombo_speed = 5
 
@@ -78,14 +99,23 @@ while running:
     elif bombo_y < 0 - bombo_size:
         bombo_y = SCREEN_HEIGHT  # Reappear on the bottom
 
+
     # Drawing everything
     screen.fill(WHITE)
     pygame.draw.rect(screen, BLUE, (player_x, player_y, player_size, player_size))
     pygame.draw.rect(screen, RED, (bombo_x, bombo_y, bombo_size, bombo_size))
     
+    #score laten zien
+    show_score(textX, textY)
+    
+    #tijdbord
+    am_ticks = int(pygame.time.get_ticks()/1000)
+    show_tijd(tijdX, tijdY)
+    
     # Update the display
-    pygame.display.flip()
+    pygame.display.update()
 
+   
     # Control the frame rate
     clock.tick(60)
 
